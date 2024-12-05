b35647bc0f3a3dce63962d37d6c73030cd40bcc7
Yeco2003 <166925873+Yeco2003@users.noreply.github.com>
2024-12-05 16:39:24 -0500

    Update 4ejemplo.py

--- sesion-3/4ejemplo.py
+++ sesion-3/4ejemplo.py
@@ -19,3 +19,34 @@ datos = pd.read_csv(archivo, sep=",", header = 0)
 #print(datos.head(7)) #muestra las primeras filas
 #print(datos.tail()) # retorna las ultimas filas
 #print(datos.sample()) #retorna filas al azar
+
+print(datos.shape) # muestra el total de filas y colomnas
+
+print (datos.size) # muestra el total de datos filas X columnas
+
+print (datos.info()) # muestra los valores organizados, muestra los valores no nulos
+
+print(datos.head(7)) # muestra los primeros datos
+
+print (datos.tail()) # muestra los ultimos datos
+
+print (datos.sample()) # muestra una fila al azar
+
+print (datos.describe()) # muestra un resumen de los datos y genera estadisticas
+
+datos.columns #muestra todas las columnas que tiene el formato, todos los nombres de las columanas
+
+datos.columns = ['latitud', 'longitud', 'housing_median_age', 'total_rooms',
+       'total_bedrooms', 'population', 'households', 'median_income',
+       'median_house_value'] # podemos editar los valores de las columnas, cambiar los nombres y el orden en que aparezcan 
+datos.info()
+
+datos['latitud'].count() # trae el total que hay en la columna a la cual hagamos el llamado
+
+datos['latitud'].value_counts() # crea tablas y nos marca cauntas veces se repite un valor en todo el formato 
+
+datos['latitude'].unique() # llama a toods las valores unicos
+
+datos.sort_values(['latitud','longitud','median_income']) # crea diagramas de los valores que hagamos el llamado 
+
+
