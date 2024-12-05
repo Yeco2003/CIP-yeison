a39c7095b6475bfd31947faa8d1eb94bacb8057e
Yeco2003 <166925873+Yeco2003@users.noreply.github.com>
2024-12-04 20:00:15 -0500

    Update arraymultidimencionales.py

--- sesion 2/arraymultidimencionales.py
+++ sesion 2/arraymultidimencionales.py
@@ -11,3 +11,9 @@ array_multidimencional3 [0][0:3] #con esta muestro la fila que quiero que se mue
 array_columna=array_multidimencional3 [:2,0:3] # llama las filas que quiero que se haga el llamado y llamo desde que columna quiero obtener los valores 
 print(array_columna)
 print()
+arraymax=array_multidimencional3.max() #el numero mayor de la matrix
+arrayresta=array_multidimencional3.min() # el numero menor de la matrix 
+arraysuma=array_multidimencional3.sum() # la suma de todos los numeros de la matrix 
+arraymaxaxis=array_multidimencional3.max(axis=0) # los numeros mayores de las columnas - filas
+arrayminaxis=array_multidimencional3.min(axis=1) # los numeros menores de las colomnas - filas
+arraysumaxis=array_multidimencional3.sum(axis=0) # la suma de los numeros de las columnas - filas 
